0cda00b9d1317f5741d7f16d1d1883e6e2dfcba4
LeeJinWoo <krebye@naver.com>
2023-10-20 14:40:15 +0900

    Feat : Project Setting
    프로젝트 세팅 작업

---
+++ CityFarmer/Assets/Script/Encyclopedia.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Encyclopedia : MonoBehaviour
+{
+    // TODO : 농작물 종류, 설명 작업
+    public int CurrentCollectionCrops { get; private set; }
+    public int MaxCollectionCrops { get; private set; }
+}
